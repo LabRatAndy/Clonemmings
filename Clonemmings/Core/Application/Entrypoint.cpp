bac9915b2656267fdaa7d60ddc274f87ab09974f
LabRatAndy <39016522+LabRatAndy@users.noreply.github.com>
2023-01-15 23:36:18 +0000

    entrypoint initial

--- Clonemmings/Core/Application/Entrypoint.cpp
+++ Clonemmings/Core/Application/Entrypoint.cpp
@@ -1,8 +1,21 @@
 #include "Core/Log/Log.h"
+#include "Core/Application/Application.h"
+
 
 int main(int argc, char** argv)
 {
 	Clonemmings::Log::Init();
 	TRACE("Logging initialised!");
+	
+	INFO("Creating Applcation object");
+	Clonemmings::Application* app = nullptr;
+	Clonemmings::CommandLineArguements args;
+	args.Args = argv;
+	args.Count = argc;
+	app = new Clonemmings::Application("Clonemmings", args);
+	INFO("Running Application");
+	app->Run();
+	INFO("Shuting down, deleting application");
+	delete app;
 	return 0;
 }
