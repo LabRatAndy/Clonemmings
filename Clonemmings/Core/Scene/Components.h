24a6a5f34f011d936d55c4b449a1d3d94bff7917
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2023-01-22 17:03:32 +0000

    Added components currently only Tag component

---
+++ Clonemmings/Core/Scene/Components.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Core/Renderer/Texture.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/quaternion.hpp>
+
+namespace Clonemmings
+{
+	struct TagComponent
+	{
+		std::string Tag;
+
+		TagComponent() = default;
+		TagComponent(const TagComponent&) = default;
+		TagComponent(const std::string& tag) : Tag(tag) {}
+	};
+
+}
