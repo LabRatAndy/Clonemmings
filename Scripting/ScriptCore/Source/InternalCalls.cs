f9816dd8a67e390e0a9bf3b13010f8b0a93e1e75
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2023-04-07 23:17:56 +0100

    added internal calls for rotation, scale of transform component. Added internal calls for game component

--- Scripting/ScriptCore/Source/InternalCalls.cs
+++ Scripting/ScriptCore/Source/InternalCalls.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Clonemmings;
+using System;
 using System.Runtime.CompilerServices;
+
 
 namespace Core
 {
     public static class InternalCalls
     {
+        //core stuff
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static bool Entity_HasComponent(ulong entityID, Type componenttype);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
@@ -15,6 +18,14 @@ namespace Core
         internal extern static void TransformComponent_GetTranslation(ulong entityID, out Vector3 translation);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static void TransformComponent_SetTranslation(ulong entityID, ref Vector3 translation);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void TransformComponent_GetRotation(ulong entityID, out Vector3 rotation);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void TransformComponent_SetRotation(ulong entityID, ref Vector3 rotation);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void TransformComponent_GetScale(ulong entityID, out Vector3 scale);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void TransformComponent_SetScale(ulong entityID, ref Vector3 scale);       
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static void RigidBody2DComponent_ApplyLinearImpulse(ulong entityID, ref Vector2 impulse, ref Vector2 point, bool wake);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
@@ -27,5 +38,24 @@ namespace Core
         internal extern static void RigidBody2DComponent_SetType(ulong entityID, RigidBody2DComponent.BodyType type);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static bool Input_IsKeyDown(KeyCode keycode);
+
+
+        //game components 
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static ClonemmingComponent.ClonmmingStatus ClonemmingComponent_GetStatus(ulong entityID);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void ClonemmingComponent_SetStatus(ulong entityID, ClonemmingComponent.ClonmmingStatus status);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static int ClonemmingStartComponent_GetNumberOfClonemmings(ulong entityID);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void ClonemmingStartComponent_SetNumberOfClonemmings(ulong entityID, int numberofclonemmings);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static int ClonemmingStartComponent_GetClonemmingReleaseRate(ulong entityID);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void ClonemmingStartComponent_SetClonemmingReleaseRate(ulong entityID, int releaserate);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static int ClonemmingExitComponent_GetNumberOfClonemmings(ulong entityID);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void ClonemmingExitComponent_SetNumberOfClonemmings(ulong entityID, int numberofclonemmings);
     }
 }
