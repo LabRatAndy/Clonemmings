e56f0bdce8da84b44658085c2bc24e49a13ab666
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2023-04-16 15:45:41 +0100

    Fix issue with mono namespace should be core not clonemmings and corrected several typos with internal call function names. Added create new entity and create new clonemminng, Destroy entity, sprite renderer component colour and tiling factor, Clonemming component walk, dig and mine speed internal call functions. Added internal calls that weren't exposed to mono.

--- Clonemmings/Core/Scripting/ScriptGlue.cpp
+++ Clonemmings/Core/Scripting/ScriptGlue.cpp
@@ -18,15 +18,16 @@ namespace Clonemmings
 {
 	static std::unordered_map<MonoType*, std::function<bool(Entity)>> s_EntityHasComponentFuncs;
 
-#define ADD_INTERNAL_CALL(name) mono_add_internal_call("Clonemmings.InternalCalls::"#name, name)
+#define ADD_INTERNAL_CALL(name) mono_add_internal_call("Core.InternalCalls::"#name, name)
 
-	static void NativeLog(MonoString* string, int parameter)
+	static void Native_Log(MonoString* string)
 	{
 		char* cStr = mono_string_to_utf8(string);
 		std::string str(cStr);
 		mono_free(cStr);
-		std::cout << str << " , " << parameter << std::endl;
+		WARN(str);
 	}
+#pragma region Vector3 functions
 	static void Vector3_Normalise(glm::vec3* parameter, glm::vec3* outresult)
 	{
 		*outresult = glm::normalize(*parameter);
@@ -35,10 +36,12 @@ namespace Clonemmings
 	{
 		return glm::dot(*parameter, *parameter);
 	}
+#pragma endregion
 	static MonoObject* GetInstance(UUID entityID)
 	{
 		return ScriptEngine::GetManagedInstance(entityID);
 	}
+#pragma region Entity  
 	static bool Entity_HasComponent(UUID uuid, MonoReflectionType* componenttype)
 	{
 		Scene* scene = ScriptEngine::GetSceneContext();
@@ -62,6 +65,64 @@ namespace Clonemmings
 		}
 		return entity.GetUUID();
 	}
+	static uint64_t Entity_CreateNewEntity(MonoString* name)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		char* cStr = mono_string_to_utf8(name);
+		std::string entityname(cStr);
+		mono_free(cStr);
+		Entity entity = scene->CreateEntity(entityname);
+		ScriptEngine::OnCreateEntity(entity);
+		return entity.GetUUID();
+	}
+	static uint64_t Entity_CreateNewClonemming(MonoString* name)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity clonemming = scene->CreateEntity();
+		if (name != nullptr)
+		{
+			char* cStr = mono_string_to_utf8(name);
+			std::string clonemmingname(cStr);
+			mono_free(cStr);
+			clonemming.GetComponent<TagComponent>().Tag = clonemmingname;
+		}
+		//clonemming component
+		auto& cc = clonemming.AddComponent<ClonemmingComponent>();
+		auto& src = clonemming.AddComponent<SpriteRendererComponent>();
+		src.Colour = { 0.100689799, 0.729729772, 0.0957946256, 1.0 };
+		src.TilingFactor = 1.0f;
+		src.Tex = nullptr;
+		auto& rb2d = clonemming.AddComponent<RigidBody2DComponent>();
+		rb2d.Type = RigidBody2DComponent::BodyType::Dynamic;
+		rb2d.FixedRotation = false;
+		auto& bc2d = clonemming.AddComponent<BoxCollider2DComponent>();
+		bc2d.Offset = { 0.0,0.0 };
+		bc2d.Size = { 0.5,0.5 };
+		bc2d.Density = 1;
+		bc2d.Friction = 0.5;
+		bc2d.Restitution = 0;
+		bc2d.RestitutionThreshold = 0.5;
+		auto& sc = clonemming.AddComponent<ScriptComponent>();
+		sc.ClassName = "Clonemmings.Clonemming";
+		scene->SetUpPhysicOnEntity(clonemming);
+		ScriptEngine::OnCreateEntity(clonemming);
+		return clonemming.GetUUID();
+
+	}
+	static void Entity_DestroyEntity(UUID uuid)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		scene->DestroyEntity(entity);
+	}
+#pragma endregion
+#pragma region TransformComponent 
+
+
 	static void TransformComponent_GetTranslation(UUID uuid, glm::vec3* outresult)
 	{
 		Scene* scene = ScriptEngine::GetSceneContext();
@@ -110,7 +171,9 @@ namespace Clonemmings
 		ASSERT(entity);
 		entity.GetComponent<TransformComponent>().Scale = *scale;
 	}
-	static void RigidBody2D_ApplyLinearImpulse(UUID uuid, glm::vec2* impulse, glm::vec2* point, bool wake)
+#pragma endregion
+#pragma region RigidBody2DComponent
+	static void RigidBody2DComponent_ApplyLinearImpulse(UUID uuid, glm::vec2* impulse, glm::vec2* point, bool wake)
 	{
 		Scene* scene = ScriptEngine::GetSceneContext();
 		ASSERT(scene);
@@ -120,7 +183,7 @@ namespace Clonemmings
 		b2Body* body = (b2Body*)rb2d.RuntimeBody;
 		body->ApplyLinearImpulse(b2Vec2(impulse->x, impulse->y), b2Vec2(point->x, point->y), wake);
 	}
-	static void RigidBody2D_ApplyLinearImpulseToCentre(UUID uuid, glm::vec2* impulse, bool wake)
+	static void RigidBody2DComponent_ApplyLinearImpulseToCentre(UUID uuid, glm::vec2* impulse, bool wake)
 	{
 		Scene* scene = ScriptEngine::GetSceneContext();
 		ASSERT(scene);
@@ -130,7 +193,7 @@ namespace Clonemmings
 		b2Body* body = (b2Body*)rb2d.RuntimeBody;
 		body->ApplyLinearImpulseToCenter(b2Vec2(impulse->x, impulse->y), wake);
 	}
-	static void RigidBody2D_GetLinearVelocity(UUID uuid, glm::vec2* outresult)
+	static void RigidBody2DComponent_GetLinearVelocity(UUID uuid, glm::vec2* outresult)
 	{
 		Scene* scene = ScriptEngine::GetSceneContext();
 		ASSERT(scene);
@@ -141,7 +204,7 @@ namespace Clonemmings
 		const b2Vec2& linearvelocity = body->GetLinearVelocity();
 		*outresult = glm::vec2(linearvelocity.x, linearvelocity.y);
 	}
-	static RigidBody2DComponent::BodyType RigidBody2D_GetType(UUID uuid)
+	static RigidBody2DComponent::BodyType RigidBody2DComponent_GetType(UUID uuid)
 	{
 		Scene* scene = ScriptEngine::GetSceneContext();
 		ASSERT(scene);
@@ -151,7 +214,7 @@ namespace Clonemmings
 		b2Body* body = (b2Body*)rb2d.RuntimeBody;
 		return Utills::Rigidbody2DTypeFromBox2DBody(body->GetType());
 	}
-	static void RigidBody2D_SetType(UUID uuid, RigidBody2DComponent::BodyType bodytype)
+	static void RigidBody2DComponent_SetType(UUID uuid, RigidBody2DComponent::BodyType bodytype)
 	{
 		Scene* scene = ScriptEngine::GetSceneContext();
 		ASSERT(scene);
@@ -161,10 +224,47 @@ namespace Clonemmings
 		b2Body* body = (b2Body*)rb2d.RuntimeBody;
 		body->SetType(Utills::Rigidbody2DTypeToBox2DBody(bodytype));
 	}
+#pragma endregion
+#pragma region SpriteRendererComponent
+	static void SpriteRendererComponent_GetColour(UUID uuid, glm::vec4* outresult)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity  entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		*outresult = entity.GetComponent<SpriteRendererComponent>().Colour;
+	}
+	static void SpriteRendererComponent_SetColour(UUID uuid, glm::vec4* colour)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		entity.GetComponent<SpriteRendererComponent>().Colour = *colour;
+	}
+	static float SpriteRendererComponent_GetTilingFactor(UUID uuid)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		return entity.GetComponent<SpriteRendererComponent>().TilingFactor;
+	}
+	static void SpriteRendererComponent_SetTilingFactor(UUID uuid, float tilingfactor)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		entity.GetComponent<SpriteRendererComponent>().TilingFactor = tilingfactor;
+	}
+#pragma endregion
 	static bool Input_IsKeyDown(KeyCode keycode)
 	{
 		return Input::IsKeyPressed(keycode);
 	}
+	// game component stuff
+#pragma region ClonemmingComponent
 	static ClonemmingComponent::ClonemingStatus ClonemmingComponent_GetStatus(UUID uuid)
 	{
 		Scene* scene = ScriptEngine::GetSceneContext();
@@ -181,6 +281,56 @@ namespace Clonemmings
 		ASSERT(entity);
 		entity.GetComponent<ClonemmingComponent>().Status = status;
 	}
+	static float ClonemmingComponent_GetWalkSpeed(UUID uuid)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		return entity.GetComponent<ClonemmingComponent>().WalkSpeed;
+	}
+	static void ClonemmingComponent_SetWalkSpeed(UUID uuid, float walkspeed)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		entity.GetComponent<ClonemmingComponent>().WalkSpeed = walkspeed;
+	}
+	static float ClonemmingComponent_GetDigSpeed(UUID uuid)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		return entity.GetComponent<ClonemmingComponent>().DigSpeed;
+	}
+	static void ClonemmingComponent_SetDigSpeed(UUID uuid, float digspeed)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		entity.GetComponent<ClonemmingComponent>().DigSpeed = digspeed;
+	}
+	static float ClonemmingComponent_GetMineSpeed(UUID uuid)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		return entity.GetComponent<ClonemmingComponent>().MineSpeed;
+	}
+	static void ClonemmingComponent_SetMineSpeed(UUID uuid, float minespeed)
+	{
+		Scene* scene = ScriptEngine::GetSceneContext();
+		ASSERT(scene);
+		Entity entity = scene->GetEntityByUUID(uuid);
+		ASSERT(entity);
+		entity.GetComponent<ClonemmingComponent>().MineSpeed = minespeed;
+	}
+#pragma endregion
+#pragma region ClonemmingStartComponent
 	static uint32_t ClonemmingStartComponent_GetNumberOfClonemmings(UUID uuid)
 	{
 		Scene* scene = ScriptEngine::GetSceneContext();
@@ -213,6 +363,8 @@ namespace Clonemmings
 		ASSERT(entity);
 		entity.GetComponent<ClonemmingStartComponent>().ClonemmingReleaseRate = releaserate;
 	}
+#pragma endregion
+#pragma region ClonemmingExitComponent
 	static uint32_t ClonemmingExitComponent_GetNumberOfClonemmings(UUID uuid)
 	{
 		Scene* scene = ScriptEngine::GetSceneContext();
@@ -229,6 +381,7 @@ namespace Clonemmings
 		ASSERT(entity);
 		entity.GetComponent<ClonemmingExitComponent>().NumberOfClonemmings = numberofclonemming;
 	}
+#pragma endregion
 	template<typename... component>
 	static void RegisterComponent()
 	{
@@ -241,7 +394,7 @@ namespace Clonemmings
 				MonoType* managedtype = mono_reflection_type_from_name(managedtypename.data(), ScriptEngine::GetCoreAssemblyImage());
 				if (!managedtype)
 				{
-					managedtypename = fmt::format("Clonemming.{}", structname);
+					managedtypename = fmt::format("Clonemmings.{}", structname);
 					managedtype = mono_reflection_type_from_name(managedtypename.data(), ScriptEngine::GetCoreAssemblyImage());
 					if (!managedtype)
 					{
@@ -264,16 +417,29 @@ namespace Clonemmings
 	}
 	void ScriptGlue::RegisterFunctions()
 	{
-		ADD_INTERNAL_CALL(NativeLog);
+		ADD_INTERNAL_CALL(Native_Log);
 		ADD_INTERNAL_CALL(Vector3_Normalise);
 		ADD_INTERNAL_CALL(Vector3_Dot);
+		ADD_INTERNAL_CALL(Entity_CreateNewClonemming);
+		ADD_INTERNAL_CALL(Entity_CreateNewEntity);
+		ADD_INTERNAL_CALL(Entity_HasComponent);
+		ADD_INTERNAL_CALL(Entity_FindEntityByName);
+		ADD_INTERNAL_CALL(Entity_DestroyEntity);
 		ADD_INTERNAL_CALL(TransformComponent_GetTranslation);
 		ADD_INTERNAL_CALL(TransformComponent_SetTranslation);
-		ADD_INTERNAL_CALL(RigidBody2D_ApplyLinearImpulse);
-		ADD_INTERNAL_CALL(RigidBody2D_ApplyLinearImpulseToCentre);
-		ADD_INTERNAL_CALL(RigidBody2D_GetLinearVelocity);
-		ADD_INTERNAL_CALL(RigidBody2D_GetType);
-		ADD_INTERNAL_CALL(RigidBody2D_SetType);
+		ADD_INTERNAL_CALL(TransformComponent_GetRotation);
+		ADD_INTERNAL_CALL(TransformComponent_SetRotation);
+		ADD_INTERNAL_CALL(TransformComponent_GetScale);
+		ADD_INTERNAL_CALL(TransformComponent_SetScale);
+		ADD_INTERNAL_CALL(RigidBody2DComponent_ApplyLinearImpulse);
+		ADD_INTERNAL_CALL(RigidBody2DComponent_ApplyLinearImpulseToCentre);
+		ADD_INTERNAL_CALL(RigidBody2DComponent_GetLinearVelocity);
+		ADD_INTERNAL_CALL(RigidBody2DComponent_GetType);
+		ADD_INTERNAL_CALL(RigidBody2DComponent_SetType);
+		ADD_INTERNAL_CALL(SpriteRendererComponent_GetColour);
+		ADD_INTERNAL_CALL(SpriteRendererComponent_SetColour);
+		ADD_INTERNAL_CALL(SpriteRendererComponent_GetTilingFactor);
+		ADD_INTERNAL_CALL(SpriteRendererComponent_SetTilingFactor);
 		ADD_INTERNAL_CALL(Input_IsKeyDown);
 		ADD_INTERNAL_CALL(TransformComponent_GetRotation);
 		ADD_INTERNAL_CALL(TransformComponent_SetRotation);
@@ -281,12 +447,18 @@ namespace Clonemmings
 		ADD_INTERNAL_CALL(TransformComponent_SetScale);
 		ADD_INTERNAL_CALL(ClonemmingComponent_GetStatus);
 		ADD_INTERNAL_CALL(ClonemmingComponent_SetStatus);
+		ADD_INTERNAL_CALL(ClonemmingComponent_GetWalkSpeed);
+		ADD_INTERNAL_CALL(ClonemmingComponent_SetWalkSpeed);
+		ADD_INTERNAL_CALL(ClonemmingComponent_GetDigSpeed);
+		ADD_INTERNAL_CALL(ClonemmingComponent_SetDigSpeed);
+		ADD_INTERNAL_CALL(ClonemmingComponent_GetMineSpeed);
+		ADD_INTERNAL_CALL(ClonemmingComponent_SetMineSpeed);
 		ADD_INTERNAL_CALL(ClonemmingStartComponent_GetNumberOfClonemmings);
 		ADD_INTERNAL_CALL(ClonemmingStartComponent_SetNumberOfClonemmings);
 		ADD_INTERNAL_CALL(ClonemmingStartComponent_GetClonemmingReleaseRate);
 		ADD_INTERNAL_CALL(ClonemmingStartComponent_SetClonemmingReleaseRate);
 		ADD_INTERNAL_CALL(ClonemmingExitComponent_GetNumberOfClonemmings);
 		ADD_INTERNAL_CALL(ClonemmingExitComponent_SetNumberOfClonemmings);
-
+		ADD_INTERNAL_CALL(GetInstance);
 	}
 }
