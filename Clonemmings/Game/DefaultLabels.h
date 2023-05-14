ed3799ce7ca16589fa2296b9a9f84d1a3c42fcfc
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2023-05-14 21:10:39 +0100

    Set up some default label text for UI elements also allow them to be serialised / deserialised

---
+++ Clonemmings/Game/DefaultLabels.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <string>
+
+namespace Clonemmings
+{
+	constexpr uint32_t CONTROLPANELLABEL = 0;
+	constexpr uint32_t LEVELCONTROLSLABEL = 1;
+	constexpr uint32_t STARTLEVELBUTTON = 2;
+	constexpr uint32_t PAUSELEVELBUTTON = 3;
+	constexpr uint32_t OPENLEVELBUTTON = 4;
+	constexpr uint32_t QUITBUTTON = 5;
+	constexpr uint32_t GAMECONTROLLEVEL = 6;
+	constexpr uint32_t RELEASERATEDRAGFLOAT = 7;
+	constexpr uint32_t NUMFLOATERSLEFTLABEL = 8;
+	constexpr uint32_t NUMBLOCKERSLEFTLABEL = 9;
+	constexpr uint32_t NUMDIGGERSLEFTLABEL = 10;
+	constexpr uint32_t NUMMINERSLEFTLABEL = 11;
+	constexpr uint32_t MAKEFLOATERBUTTON = 12;
+	constexpr uint32_t MAKEBLOCKERBUTTON = 13;
+	constexpr uint32_t MAKEDIGGERBUTTON = 14;
+	constexpr uint32_t MAKEMINERBUTTON = 15;
+	constexpr uint32_t MAKEWALKERBUTTON = 16;
+	constexpr uint32_t STATISTICSLABEL = 17;
+	constexpr uint32_t CLONEMMINGSSAVEDLABEL = 18;
+	constexpr uint32_t CLONEMMINGSLOSTLABEL = 19;
+
+#ifdef DEBUG
+	#define CONTROLPANELLABELTEXT "Control Panel"
+	#define LEVELCONTROLSLABELTEXT "Level Controls"
+	#define STARTLEVELBUTTONTEXT "Start Level"
+	#define PAUSELEVELBUTTONTEXT "Pause Level"
+	#define OPENLEVELBUTTONTEXT "Open Level"
+	#define QUITBUTTONTEXT "Quit"
+	#define GAMECONTROLLEVELTEXT "Game Controls"
+	#define RELEASERATEDRAGFLOATTEXT "Clonemming Release Rate"
+	#define NUMFLOATERSLEFTLABELTEXT "Number of floaters left"
+	#define NUMBLOCKERSLEFTLABELTEXT "Number of blockers left"
+	#define NUMDIGGERSLEFTLABELTEXT "Number of diggers left"
+	#define NUMMINERSLEFTLABELTEXT "Number of miners left"
+	#define MAKEFLOATERBUTTONTEXT "Make Floater"
+	#define MAKEBLOCKERBUTTONTEXT "Make Blocker"
+	#define MAKEDIGGERBUTTONTEXT "Make Digger"
+	#define MAKEMINERBUTTONTEXT "Make Miner"
+	#define MAKEWALKERBUTTONTEXT "Make Walker"
+	#define STATISTICSLABELTEXT "Level Statistics"
+	#define CLONEMMINGSSAVEDLABELTEXT "Number of clonemmings saved"
+	#define CLONEMMINGSLOSTLABELTEXT "Number of clonemmings lost"
+#endif // DEBUG
+
+}
---
+++ Clonemmings/Game/LabelSerialiser.cpp
@@ -0,0 +1,83 @@
+#include "Game/LabelSerialiser.h"
+#include "Game/ControlPanel.h"
+
+#include <yaml-cpp/yaml.h>
+
+#include <ostream>
+#include <filesystem>
+
+namespace Clonemmings
+{
+	static void CheckAndCreateDirectory(const std::string& filepath)
+	{
+		std::filesystem::path path(filepath);
+		std::filesystem::path directory = path.parent_path();
+		if (!std::filesystem::is_directory(directory))
+		{
+			std::filesystem::create_directory(directory);
+		}
+	}
+
+	LabelSerialiser::LabelSerialiser(ControlPanel* controlpanel) : m_ControlPanelPtr(controlpanel)
+	{
+
+	}
+
+	void LabelSerialiser::Serialise(const std::string& filename)
+	{
+		YAML::Emitter out;
+		out << YAML::BeginMap;
+		out << YAML::Key << "LabelText" << YAML::Value << "Label strings for Clonemmings control panel";
+		out << YAML::Key << "labels" << YAML::Value << YAML::BeginSeq;
+		uint32_t count = 0;
+		for (auto it = m_ControlPanelPtr->m_LabelMap.begin(); it != m_ControlPanelPtr->m_LabelMap.end(); ++it)
+		{
+			out << YAML::Key << std::to_string(count);
+			out << YAML::Key << YAML::BeginMap;
+			out << YAML::Key << "LabelID" << YAML::Value << it->first;
+			LabelText text = it->second;
+			out << YAML::Key << "LabelText" << YAML::Value << text.Text;
+			out << YAML::EndMap;
+			count++;
+		}
+		out << YAML::EndSeq;
+		out << YAML::EndMap;
+		CheckAndCreateDirectory(filename);
+		std::ofstream output(filename.c_str(), std::ios::out | std::ios::trunc);
+		if (output.is_open())
+		{
+			output << out.c_str();
+		}
+		output.close();
+	}
+
+	bool LabelSerialiser::Deserialise(const std::string& filename)
+	{
+		YAML::Node data;
+		
+		try
+		{
+			data = YAML::LoadFile(filename);
+		}
+		catch (YAML::ParserException e)
+		{
+			return false;
+		}
+
+		if (!data["LabelText"])
+			return false;
+		auto labels = data["Labels"];
+		if (labels)
+		{
+			m_ControlPanelPtr->m_LabelMap.clear();
+			for (auto label : labels)
+			{
+				uint32_t labelID = label["LabelID"].as<uint32_t>();
+				std::string labeltext = label["LabelText"].as<std::string>();
+				LabelText text(labeltext);
+				m_ControlPanelPtr->m_LabelMap[labelID] = text;
+			}
+		}
+		return true;
+	}
+}
